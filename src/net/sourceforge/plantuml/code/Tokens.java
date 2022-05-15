b65c5136e47d93ff8d1c5a2499b97a324d8c2c05
Alex5041 <kononal@gmail.com>
2022-05-15 16:16:40 +0300

    verbose ```add``` changed to cycle ```add```

--- src/net/sourceforge/plantuml/code/Tokens.java
+++ src/net/sourceforge/plantuml/code/Tokens.java
@@ -5,12 +5,12 @@
  * (C) Copyright 2009-2023, Arnaud Roques
  *
  * Project Info:  http://plantuml.com
- * 
+ *
  * If you like this project or if you find it useful, you can support us at:
- * 
+ *
  * http://plantuml.com/patreon (only 1$ per month!)
  * http://plantuml.com/paypal
- * 
+ *
  * This file is part of PlantUML.
  *
  * PlantUML is free software; you can redistribute it and/or modify it
@@ -31,11 +31,12 @@
  *
  * Original Author:  Arnaud Roques
  *
- * 
+ *
  */
 package net.sourceforge.plantuml.code;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -43,6 +44,124 @@ import java.util.TreeSet;
 public class Tokens {
 
 	private final List<String> keywords = new ArrayList<>();
+	private final List<String> added = Arrays.asList("actor",
+					"participant",
+					"usecase",
+					"class",
+					"interface",
+					"abstract",
+					"enum",
+					"component",
+					"state",
+					"object",
+					"artifact",
+					"folder",
+					"rectangle",
+					"node",
+					"frame",
+					"cloud",
+					"database",
+					"storage",
+					"agent",
+					"stack",
+					"boundary",
+					"control",
+					"entity",
+					"card",
+					"file",
+					"package",
+					"queue",
+					"archimate",
+					"diamond",
+					"detach",
+					"@start",
+					"@end",
+					"also",
+					"autonumber",
+					"caption",
+					"title",
+					"newpage",
+					"loop",
+					"break",
+					"critical",
+					"note",
+					"legend",
+					"group",
+					"left",
+					"right",
+					"link",
+					"over",
+					"activate",
+					"deactivate",
+					"destroy",
+					"create",
+					"footbox",
+					"hide",
+					"show",
+					"skinparam",
+					"skin",
+					"bottom",
+					"namespace",
+					"page",
+					"down",
+					"else",
+					"endif",
+					"partition",
+					"footer",
+					"header",
+					"center",
+					"rotate",
+					"return",
+					"repeat",
+					"start",
+					"stop",
+					"while",
+					"endwhile",
+					"fork",
+					"again",
+					"kill",
+					"order",
+					"mainframe",
+					"across",
+					"stereotype",
+					"split",
+					"style",
+					"sprite",
+					"exit",
+					"include",
+					"pragma",
+					"undef",
+					"ifdef",
+					"endif",
+					"ifndef",
+					"else",
+					"function",
+					"procedure",
+					"endfunction",
+					"endprocedure",
+					"unquoted",
+					"return",
+					"startsub",
+					"endsub",
+					"assert",
+					"local",
+					"!definelong",
+					"!enddefinelong",
+					"!define",
+					"define",
+					"alias",
+					"shape",
+					"label",
+					"BackgroundColor",
+					"Color",
+					"color",
+					"Entity",
+					"ENTITY",
+					"COLOR",
+					"LARGE",
+					"stereo",
+					"AZURE",
+					"Azure");
 
 	public static void main(String[] args) {
 		System.err.println("keywords=" + new Tokens().keywords.size());
@@ -69,129 +188,12 @@ public class Tokens {
 	}
 
 	public Tokens() {
-		add("actor");
-		add("participant");
-		add("usecase");
-		add("class");
-		add("interface");
-		add("abstract");
-		add("enum");
-		add("component");
-		add("state");
-		add("object");
-		add("artifact");
-		add("folder");
-		add("rectangle");
-		add("node");
-		add("frame");
-		add("cloud");
-		add("database");
-		add("storage");
-		add("agent");
-		add("stack");
-		add("boundary");
-		add("control");
-		add("entity");
-		add("card");
-		add("file");
-		add("package");
-		add("queue");
-		add("archimate");
-		add("diamond");
-		add("detach");
-
-		add("@start");
-		add("@end");
-		add("also");
-		add("autonumber");
-		add("caption");
-		add("title");
-		add("newpage");
-		add("loop");
-		add("break");
-		add("critical");
-		add("note");
-		add("legend");
-		add("group");
-		add("left");
-		add("right");
-		add("link");
-		add("over");
-		add("activate");
-		add("deactivate");
-		add("destroy");
-		add("create");
-		add("footbox");
-		add("hide");
-		add("show");
-		add("skinparam");
-		add("skin");
-		add("bottom");
-		add("namespace");
-		add("page");
-		add("down");
-		add("else");
-		add("endif");
-		add("partition");
-		add("footer");
-		add("header");
-		add("center");
-		add("rotate");
-		add("return");
-		add("repeat");
-		add("start");
-		add("stop");
-		add("while");
-		add("endwhile");
-		add("fork");
-		add("again");
-		add("kill");
-		add("order");
-		add("mainframe");
-		add("across");
-		add("stereotype");
-		add("split");
-		add("style");
-		add("sprite");
-
-		add("exit");
-		add("include");
-		add("pragma");
-		add("undef");
-		add("ifdef");
+		for(String element:added){
+			add(element);
+		}
 		// add("endif");
-		add("ifndef");
 		// add("else");
-		add("function");
-		add("procedure");
-		add("endfunction");
-		add("endprocedure");
-		add("unquoted");
 		// add("return");
-		add("startsub");
-		add("endsub");
-		add("assert");
-		add("local");
-
-		add("!definelong");
-		add("!enddefinelong");
-		add("!define");
-
-		add("define");
-		add("alias");
-		add("shape");
-		add("label");
-		add("BackgroundColor");
-		add("Color");
-		add("color");
-		add("Entity");
-		add("ENTITY");
-		add("COLOR");
-		add("LARGE");
-		add("stereo");
-		add("AZURE");
-		add("Azure");
-
 	}
 
 	private void add(String string) {
